31beee690060fb88085db53896e444b8eec9ae4b
Charly <charlyplanquettepro@gmail.com>
2024-12-03 23:16:03 +0100

    first commit

---
+++ models/index.js
@@ -0,0 +1,16 @@
+//configuration de sequelize
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+//connexion bdd avec infos
+const sequelize = new Sequelize(process.env.DATABASE_NAME,
+    process.env.ID_DATABASE,
+    process.env.PASSWORD_DATABASE,
+    {    dialect: process.env.DIALECT_DATABASE,
+        host: process.env.HOST_DATABSE
+    });
+// const sequelize = new Sequelize('sebi', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const app = require('./app');
+const port = 8080;
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+
+app.listen(port, () => {
+    console.log(`Server is listening on port http://localhost:${port}`)
+});
